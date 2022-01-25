76268b2ec37f8aecee3345d867f51f4dcfa1ffbe
inord777dev <inord777dev@gmail.com>
2022-01-25 15:49:56 +0300

    init: self-study

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,22 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
+
+const initialState = []
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  }
+})
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users')
+  return response.data
+})
+
+export default usersSlice.reducer
